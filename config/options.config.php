52b41ee2d2c089e2cc1fe9b21f37042cdf9c6c8f
cristian.cdi@gmail.com <Neverwinter2>
2019-03-24 12:19:41 -0300

    Se agrega que devuelva el item al crear/actualizar

--- config/options.config.php
+++ config/options.config.php
@@ -1,6 +1,9 @@
 <?php
 return [
     'zf-metal-security-rest.options' => [
-        'web_host' =>  (isset($_SERVER['HTTP_HOST']))?$_SERVER['HTTP_HOST']:"",
+        'web_host' => (isset($_SERVER['HTTP_HOST'])) ? $_SERVER['HTTP_HOST'] : "",
+    ],
+    'zf-metal-restful.options' => [
+        'return_item_on_update' => true,
     ],
 ];
