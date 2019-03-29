52c2c39a18548cc18987166f7d9df4bd4ac2cc35
cristian.cdi@gmail.com <Neverwinter2>
2019-03-29 19:48:00 -0300

    Se saca validadores y se pasan a commons

--- config/module.config.php
+++ config/module.config.php
@@ -8,6 +8,5 @@ return array_merge_recursive(
     include 'controller.config.php',
     include 'services.config.php',
     include 'plugins.config.php',
-    include 'view.config.php',
-    include 'form.config.php'
+    include 'view.config.php'
 );
