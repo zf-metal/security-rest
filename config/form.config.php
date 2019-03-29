2ef06411cdd473f17446a2cca087761db0aae2b2
cristian.cdi@gmail.com <Neverwinter2>
2019-03-29 19:17:39 -0300

    Se saca callback en initializer

--- config/form.config.php
+++ config/form.config.php
@@ -1,17 +1,6 @@
 <?php
 
 return [
-    'form_elements' => [
-        'initializers' => [
-            'ObjectManagerInitializer' => function ($container,  $formElement) {
-                if ($formElement instanceOf \DoctrineModule\Persistence\ObjectManagerAwareInterface) {
-                    $em = $container->get("doctrine.entitymanager.orm_default");
-                    $formElement->setObjectManager($em);
-                    $formElement->setHydrator(new \DoctrineModule\Stdlib\Hydrator\DoctrineObject($em));
-                }
-            }
-        ]
-    ],
     'validators' => [
         'factories' => [
             'UniqueObject' => \ZfMetal\Commons\Factory\Validator\UniqueObjectFactory::class,
